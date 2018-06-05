0855d332b1660e12019e256d64eff2986507d0c7
Hajime Fukuda <hajime.fukuda@me.com>
2018-06-05 15:07:03 +0900

    a bit more detailed code

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "classes/DelphesClasses.h"
 #include "TChain.h"
 #include "ExRootAnalysis/ExRootTreeReader.h"
+#include "TClonesArray.h"
 
 
 int main(int argc, char** argv) {
@@ -16,6 +17,18 @@ int main(int argc, char** argv) {
   chain.Add(argv[1]);
 
   ExRootTreeReader treeReader(&chain);
+
+  auto* particles = treeReader.UseBranch("Particle");
+  auto* met = treeReader.UseBranch("MissingET");
+
+  const int nEvent = treeReader.GetEntries();
+
+  for (unsigned int entry = 0; entry < nEvent; entry++) {
+    treeReader.ReadEntry(entry);
+
+    for (unsigned int i = 0; i < particles->GetEntries(); i++) {
+    }
+  }
 
   return 0;
 }
